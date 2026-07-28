d12843855b87494abc884b631bd0fbbc4b99bc5e
Juan Vázquez Longueira <vazquezjuan648@gmail.com>
2026-07-28 18:12:04 +0200

    fix(TFG-5): configurado testcontainers en test y añadida dependencia restclient #time 30m
    La versión actual de Spring-Boot ha tomado una ruta más modular y ya no trae la dependencia de restclient por defecto, lo que causaba que el test de contexto y la ejecución fallaran.

--- backend/src/test/java/com/github/kio7po/comic_tracker/ComicTrackerApplicationTests.java
+++ backend/src/test/java/com/github/kio7po/comic_tracker/ComicTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.kio7po.comic_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class ComicTrackerApplicationTests {
 
 	@Test
